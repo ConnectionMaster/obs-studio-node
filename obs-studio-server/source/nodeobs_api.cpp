2852122c1a41864858ee92413755878e8e312453
Richard <rosborne255@yahoo.com>
2026-08-26 20:54:22 -0500

    Upgrade to Electron 43 (#1765)
    * Fix OSX_DEPLOYMENT_TARGET override
    * electron 43 yarn.lock update
    * set C++ 20 standard
    * do not override OSX_DEPLOYMENT_TARGET in the build script, use setting from Cmake
    * fix C++ 20 compilation issues on Windows
    * fix CMAKE_CXX_STANDARD_REQUIRED typo
    * Fix test imports
    
    Resolve issue:
    ERROR: TypeScript import equals declaration is not supported in strip-only mode
    (node:26607) [MODULE_TYPELESS_PACKAGE_JSON] Warning: Module type of file:///Users/rosbo/projects/streamlabs/obs-studio-node/tests/osn-tests/src/test_audio_encoder.ts is not specified and it doesn't parse as CommonJS.
    Reparsing as ES module because module syntax was detected. This incurs a performance overhead.
    To eliminate this warning, add "type": "module" to /Users/rosbo/projects/streamlabs/obs-studio-node/package.json.
    (Use `Electron --trace-warnings ...` to show where the warning was created)
    
    * Upgrade streamlabs-ipc module to C++ 20

--- obs-studio-server/source/nodeobs_api.cpp
+++ obs-studio-server/source/nodeobs_api.cpp
@@ -1433,7 +1433,7 @@ VOID DisconnectAndReconnect(void)
 
 bool prepareTerminationPipe()
 {
-	LPTSTR lpszPipename = TEXT("\\\\.\\pipe\\exit-slobs-crash-handler");
+	LPCTSTR lpszPipename = TEXT("\\\\.\\pipe\\exit-slobs-crash-handler");
 
 	hEvents = CreateEvent(NULL, TRUE, TRUE, NULL);
 
